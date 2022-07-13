391f2c4495581e773231dbabe51fdd9b242fdda4
matthiasboosch <matthias_boosch@yahoo.de>
2022-07-13 15:54:27 +0200

    Initial Commit

---
+++ src/main/java/com/example/testinggithub/TestingGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.testinggithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingGitHubApplication.class, args);
+    }
+
+}
